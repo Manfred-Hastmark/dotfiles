acafc59f6fc23c066a7eebe004fa1150eeb5b10d
hastmark <hastmark2001@hotmail.com>
2025-10-22 21:14:11 +0200

    Added some more configs for neovim

--- apt/install.sh
+++ apt/install.sh
@@ -1,3 +1,4 @@
 apt install xorg xinit i3 feh
-apt install neovim tmux alacritty fzf luarocks ripgrep npm curl
+apt install neovim tmux alacritty fzf luarocks ripgrep npm curl fd-find
 npm install -g neovim
+luarocks install jsregexp
---
+++ nvim/configs/lua/plugins/vim-devicons.lua
@@ -0,0 +1,6 @@
+return {
+    "ryanoasis/vim-devicons",
+    config = function()
+        vim.cmd("set encoding=UTF-8")
+    end,
+}
